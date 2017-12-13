d984c2f2d2d566be67f2b4595771ce78b92cbfad
Sathish kumar Murugesan <sathish@Sathishs-MacBook-Pro.local>
2017-12-13 11:32:28 +0100

    First commit

---
+++ src/main/java/hello/storage/FileSystemStorageService.java
@@ -0,0 +1,117 @@
+package hello.storage;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileSystemStorageService implements StorageService {
+
+    public static final String INBOUND = "/inbound";
+    private Path rootLocation;
+
+    private final StorageProperties properties;
+
+    @Autowired
+    public FileSystemStorageService(final StorageProperties properties) {
+        this.properties = properties;
+        this.rootLocation = Paths.get(properties.getLocation());
+    }
+
+    @Override
+    public void store(MultipartFile file, String subFolderName) {
+        Path folderLocation = Paths.get(properties.getLocation() + "/" + subFolderName + INBOUND);
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        deleteFiles(folderLocation);
+        createDirectory(folderLocation);
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file " + filename);
+            }
+            if (filename.contains("..")) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file with relative path outside current directory "
+                                + filename);
+            }
+
+            Files.copy(file.getInputStream(), folderLocation.resolve(filename),
+                    StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to store file " + filename, e);
+        }
+    }
+
+    private boolean deleteFiles(Path folderLocation) {
+        return FileSystemUtils.deleteRecursively(folderLocation.toFile());
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(path -> this.rootLocation.relativize(path));
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+
+    }
+
+    @Override
+    public Path load(String filename) {
+        return rootLocation.resolve(filename);
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        try {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            }
+            else {
+                throw new StorageFileNotFoundException(
+                        "Could not read file: " + filename);
+
+            }
+        }
+        catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        deleteFiles(rootLocation);
+    }
+
+    @Override
+    public void init() {
+        createDirectory(rootLocation);
+    }
+
+    public void createDirectory(Path rootLocation){
+        try {
+            Files.createDirectories(rootLocation);
+        }
+        catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
+    }
+}
